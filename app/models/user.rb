90ae07d7ce1da56e99b30c3a055b7d050780bf2b
Dillon Estelle <email": null>
2017-05-09 11:24:41 -0700

    create user model and migrations

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :name, :email, presence: true
+  validates :email, uniqueness: true
+  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+end
